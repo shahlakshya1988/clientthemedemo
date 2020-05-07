ba5b1c0eaa628ea517cd048ceaa966be3457104f
Shah Lakshya Pradhyuman <shahlakshya1988@gmail.com>
2020-05-07 22:01:42 +0530

    More Medical Themes Added

---
+++ medical/theme-3/js/map.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+    var map = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30710983.209769644!2d64.45235976587381!3d20.01273993518969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1588868381320!5m2!1sen!2sin" width="100%" frameborder="0" style="border:0;height:inherit" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>';
+    $("#map").html(map);
+});
